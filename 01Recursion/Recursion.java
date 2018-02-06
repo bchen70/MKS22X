93991b7ae260419db465870e00183a677f00c97e
bchen70 <bchen17@stuy.edu>
2018-02-06 00:56:53 -0500

    added and tested fib

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -5,16 +5,57 @@ public class Recursion{
       throw new IllegalArgumentException();
     }
     if (n == 0){
+      return 1;
+    }
+    return fact(n-1) * n;
+  }
+
+  //1.1b Fibonacci in O(n)
+  public int fib(int n){
+    if (n < 0){
+      throw new IllegalArgumentException();
+    }
+    if (n == 0){
       return 0;
     }
-    return n + fact(n-1);
+    if (n == 1){
+      return 1;
+    }
+    return fibhelp(1,1,2,n);
+  }
+
+  public int fibhelp(int bsum, int sum, int count, int n){
+    if (count == n){
+      return sum;
+    }
+    return fibhelp(sum, bsum+sum, count + 1, n);
+  }
+
+
+  public int fib2(int n){
+    if (n < 0){
+      throw new IllegalArgumentException();
+    }
+    if (n == 0){
+      return 0;
+    }
+    if (n == 1){
+      return 1;
+    }
+    return fib2(n-1) + fib(n-2);
   }
 
   public static void main(String[] args){
     Recursion x = new Recursion();
+    //fact
     // System.out.println(x.fact(-1));returns exception
-    System.out.println(x.fact(0));
-    System.out.println(x.fact(1));
-    System.out.println(x.fact(7));
-      }
+    //System.out.println(x.fact(0)); 1
+    // System.out.println(x.fact(1)); 1
+    // System.out.println(x.fact(7)); 540
+
+    //fib
+    // System.out.println(x.fib(-1));error
+    // System.out.println(x.fib(0)); 0
+    //System.out.println(x.fib(1)); 1
+  }
 }
