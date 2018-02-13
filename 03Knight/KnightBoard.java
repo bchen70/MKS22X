5145fa85ee6320b1f2ba1c55f0644438d2e51dfa
bchen70 <bchen17@stuy.edu>
2018-02-13 15:17:37 -0500

    Set up KnightBoard class

---
+++ 03Knight/KnightBoard.java
@@ -0,0 +1,12 @@
+public class KnightBoard{
+    public KnightBoard(int startingRows,int startingCols){
+    }
+    public String toString(){
+    }
+    public boolean solve(int startingRow, int startingCol){
+    }
+    public int countSolutions(int startingRow, int startingCol){
+    }
+    private boolean solveH(int row ,int col, int level){
+    }		
+}
