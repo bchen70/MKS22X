427939862c0ff06b8cc59fff110feaf406c5c6af
bchen70 <bchen17@stuy.edu>
2018-02-13 15:23:24 -0500

    I think I set up constructor correctly???

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,12 +1,22 @@
 public class KnightBoard{
+    private int board[][];
     public KnightBoard(int startingRows,int startingCols){
+	if (startingRows < 0 && startingCols < 0){
+	    throw new IllegalArgumentException("Size cannot be less than 0");
+	}
+	board = new int [startingRows][startingCols];
     }
-    public String toString(){
+    
+    // public String toString(){
+    // }
+    // public boolean solve(int startingRow, int startingCol){
+    // }
+    // public int countSolutions(int startingRow, int startingCol){
+    // }
+    // private boolean solveH(int row ,int col, int level){
+    // }
+    public static void main(String[]args){
+	KnightBoard b = new KnightBoard(4,3);
+	System.out.println(b);
     }
-    public boolean solve(int startingRow, int startingCol){
-    }
-    public int countSolutions(int startingRow, int startingCol){
-    }
-    private boolean solveH(int row ,int col, int level){
-    }		
 }
