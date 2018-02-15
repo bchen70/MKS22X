4032eee81ab3d3ee43c6df411aeea9e0a149bcc1
bchen70 <bchen17@stuy.edu>
2018-02-15 15:11:40 -0500

    solve prob doesnt work properly but prob need to add illegalargumentexception when parameter is neg or out of bounds

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -16,26 +16,38 @@ public class KnightBoard{
 	for (int r = 0; r < startingRows; r ++){
 	    for (int c = 0; c < startingCols; c++){
 		if (board[r][c] == -1){
-		    str += "_";
+		    str += "0";
 		    //need to fix this part?? or idk??
 		    // blank boards display 0's as underscores 
 		    // you get a blank board if you never called solve or 
 		    // when there is no solution  			 
 		}
 		else{
-		    str += "0";
+		    str += "_";
 		}
 	    }
 	    str += "\n";
 	}
 	return str;
     }
-    // public boolean solve(int startingRow, int startingCol){
-    // }
+    public boolean solve(int startingRow, int startingCol){
+	for (int r=0; r < startingRows ; r++){
+	    for (int c=0; c< startingCols; c++) {
+		if (board[r][c] != 0){
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+	return solveH(0,0,0);
+    }
     // public int countSolutions(int startingRow, int startingCol){
     // }
-    // private boolean solveH(int row ,int col, int level){
-    // }
+    private boolean solveH(int row ,int col, int level){
+	if (col == startingCols){
+	    return true;
+	}
+	return false;
+    }
     public static void main(String[]args){
 	KnightBoard b = new KnightBoard(4,3);
 	System.out.println(b);
