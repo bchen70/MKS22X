a0472fadac0331427bc9baa9c91ccbfdc58dfaf8
bchen70 <bchen17@stuy.edu>
2018-03-10 22:33:19 -0500

    Ok i added a stomp but i think its gonna be a problem lol

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -19,15 +19,33 @@ public class USACO{
 		}
 	    }
 	    for (int x= 0; x< com; x++){
-		stomp(input.nextInt(),input.nextInt(),input.netInt()));
-	    // need to write stomp btw
+		stomp(input.nextInt(),input.nextInt(),input.netInt());
+	    }
+
 	}
 	catch (FileNotFoundException e){
 	    System.out.println("File not found.");
 	}
     }
-	       
-
+    
+    public void stomp(int row,int col, int level){
+	int max = lake[row- 1][col - 1];
+	for (int i = row; i < row + 3; i ++){
+	    for (int j = col; j < col +3; j++){
+		if (lake[i - 1][j-1] > max){
+		    max = lake[i - 1][j - 1];
+		}
+	    }
+	}
+	for (int x = row; x < row + 3; x++){
+	    for (int y = col; y < col + 3; y++){
+		if(lake[x - 1][y - 1] > max - level){
+		    lake [x-1][y-1] = max - level;
+		}
+	    }
+	}
+    }
+    
     public static int silver(String filename){
 	
     }
