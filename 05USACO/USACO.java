edb339a3cce85560f5b9db136ea0a1688f577528
bchen70 <bchen17@stuy.edu>
2018-03-10 22:13:53 -0500

    FINISHED CONSTRUCTOR? write stomp btw

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,8 +1,33 @@
+import java.io.*;
+import java.util.*;
+
 public class USACO{
     public static int bronze(String filename){
-	int row = 0;
-	int col = 0;
+	File line = new File(filename);
+	try {
+	    Scanner input = new Scanner(line);
+	    
+	    int row = input.nextInt();
+	    int cols = input.nextInt();
+	    int depth = input.nextInt();
+	    int com = input.nextInt();
+
+	    int[][] lake  = new int[row][cols];
+	    for (int i = 0; i <rows;i++){
+		for (int x = 0; x < cols; x ++){
+		    lake[i][x] = input.nextInt();
+		}
+	    }
+	    for (int x= 0; x< com; x++){
+		stomp(input.nextInt(),input.nextInt(),input.netInt()));
+	    // need to write stomp btw
+	}
+	catch (FileNotFoundException e){
+	    System.out.println("File not found.");
+	}
     }
+	       
+
     public static int silver(String filename){
 	
     }
