6423fe83add2e228d6388530a0bb0d328029a1e8
bchen70 <bchen17@stuy.edu>
2018-03-13 21:44:23 -0400

    maybe finish partition?

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -3,6 +3,22 @@ public class Quick{
     public static int partition(int []data,int start,int end){
 	int rand = start + (int)(Math.random() * (end - start + 1));
 	int pivot = data[rand];
+	swap(rand,start,data);
+
+	int s = start + 1;
+	int e = end;
+
+	while (s <= e){
+	    if (data[s] < pivot){
+		s++;
+	    }
+	    else if (data[s] > pivot){
+		swap(s,e,data);
+		e -- ;
+	    }
+	}
+	swap(start,e,data);
+	return e;
 	
     }
     private static void swap(int x, int y, int[] ary) {
