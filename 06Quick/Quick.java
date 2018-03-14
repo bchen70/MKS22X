cd66809e64887e93b4e95058d6903900f51bc192
bchen70 <bchen17@stuy.edu>
2018-03-13 22:12:16 -0400

    added quciksort and testing

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,11 +1,15 @@
 import java.util.*;
 public class Quick{
 
-    public static void main(String[] ary){
-	int[] data = {5,3,7,5,34,3,765};
+    public static void main(String[] args){
+	int[] ary = {5,3,7,5,34,3,765};
 
-	System.out.println(partition(data,2, data.length-1));
-
+	
+	System.out.println(Quick.quickselect(ary,0));
+	System.out.println(Quick.quickselect(ary,1));
+	System.out.println(Quick.quickselect(ary,2));
+	System.out.println(Quick.quickselect(ary,3));
+	System.out.println(Quick.quickselect(ary,4));
     }
     public static int partition(int []data,int start,int end){
 	int rand = start + (int)(Math.random() * (end - start + 1));
@@ -28,7 +32,24 @@ public class Quick{
 	return e;
 	
     }
-    private static void swap(int x, int y, int[] ary) {
+
+    public static int quickselect(int[]data,int k){
+	int start = 0;
+	int end = data.length-1;
+	for(int i =0;i<data.length; i++){
+	    if (partition(data,start,end) == k){
+		return data[(partition(data,start,end))];
+	    }
+	    if (partition(data,start,end) < k){
+		start = partition(data,start,end) + 1;
+	    }
+	    else{
+		end = partition(data,start,end) - 1;
+	    }
+	}
+	return -1;	    
+    }
+    public static void swap(int x, int y, int[] ary) {
         int temp = ary[x];
         ary[x] = ary[y];
         ary[y] = temp;
