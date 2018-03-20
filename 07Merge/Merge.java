78f3a946e18b0183facf7118639cc0f867b394df
bchen70 <bchen17@stuy.edu>
2018-03-20 15:34:07 -0400

    msort

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -17,6 +17,25 @@ public class Merge{
     }
     private static void msort(int[]data, int[]temp,int lo, int hi){
 	int mid = (lo+hi)/ 2;
+	int a = 0;
+	int b = 0;
+	while (a < data.length){
+	    
+	    if (lo >= hi){
+		return;
+	    }
+	    temp = data;
+	    if (data[lo]>=data[lo + 1]){
+		temp[a] = data[lo + 1];
+		a++;
+	    }
+	    else{
+		temp[a] = data[lo];
+		a++;
+	    }
+	}
+	    
+	    
 	msort(temp,data,lo,mid);
 	msort(temp,data,mid + 1,hi);
     }
