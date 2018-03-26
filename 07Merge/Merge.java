81f172e40a291aa1c26caa3698ea60adb7c93690
bchen70 <bchen17@stuy.edu>
2018-03-25 21:21:16 -0400

    fixed mergesort i think need to add merge?

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,5 +1,6 @@
 public class Merge{
     public static void mergesort(int[] data){
+	int[] temp = new int[data.length];
 	msort(data,temp,0,data.length-1);
     }
     private static void msort(int[]data, int[]temp,int lo, int hi){
