f121a6a2633356f0c85c8bf8d30c82a00517425a
bchen70 <bchen17@stuy.edu>
2018-03-25 21:29:51 -0400

    realized that it shuld be i<=hi2 for merge and also added merge to msort

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,7 +1,7 @@
 public class Merge{
     public static void merge(int[] data, int[] temp, int lo, int hi,int lo2, int hi2){
 	int mid = (lo+hi)/ 2;
-	for (int i = lo; i <= hi ; i++){
+	for (int i = lo; i <= hi2 ; i++){
 	    if (lo <= hi && (lo2 > hi2 || temp[lo] <= temp[lo2])){
 		data[i] = temp[lo];
 		lo ++;
@@ -26,5 +26,6 @@ public class Merge{
 	}
 	msort(temp,data,lo,mid);
 	msort(temp,data,mid + 1,hi);
+	merge(data,temp,lo,mid,mid + 1,hi);
     }
 }
