1f4b604f0a75cd3465c183ac485a62da4ed2ff6c
bchen70 <bchen17@stuy.edu>
2018-03-25 21:28:40 -0400

    finished merge

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,4 +1,18 @@
 public class Merge{
+    public static void merge(int[] data, int[] temp, int lo, int hi,int lo2, int hi2){
+	int mid = (lo+hi)/ 2;
+	for (int i = lo; i <= hi ; i++){
+	    if (lo <= hi && (lo2 > hi2 || temp[lo] <= temp[lo2])){
+		data[i] = temp[lo];
+		lo ++;
+	    }
+	    else{
+		data[i] = temp[lo2];
+		lo2++;
+	    }
+	}
+    }
+    
     public static void mergesort(int[] data){
 	int[] temp = new int[data.length];
 	msort(data,temp,0,data.length-1);
