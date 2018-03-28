46056df6d449dfc8d72db5942e46f230ac9dfccb
bchen70 <bchen17@stuy.edu>
2018-03-28 15:32:51 -0400

    huuuuuuuuuuuuuh

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -1,19 +1,34 @@
 public class MyLinkedList{
     int length;
+    Node first;
+    Node last;
     public MyLinkedList(){
 	
     }
-
+    
     public boolean add(int value){
+	data = value;
+	length = length + 1;
+	return true;
     }
 
     public int size(){
 	return length;
     }
+    
     public String toString(){
+	return "x";
     }
+    
     public int get(int index){
+	return index;
     }
-    public int set(int index,int newValue){
+    
+    public void set(int index,int newValue){
+    }
+    
+    private class Node{
+	private Node next,prev;
+	private int data;
     }
 }
