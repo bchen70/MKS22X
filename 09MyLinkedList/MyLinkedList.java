f16608f246eaece8c4476e2d13b417cd5077ee9b
bchen70 <bchen17@stuy.edu>
2018-03-27 22:18:59 -0400

    uh set up?

---
+++ 09MyLinkedList/MyLinkedList.java
@@ -0,0 +1,19 @@
+public class MyLinkedList{
+    int length;
+    public MyLinkedList(){
+	
+    }
+
+    public boolean add(int value){
+    }
+
+    public int size(){
+	return length;
+    }
+    public String toString(){
+    }
+    public int get(int index){
+    }
+    public int set(int index,int newValue){
+    }
+}
