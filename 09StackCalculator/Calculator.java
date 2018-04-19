795e84965301f04aee0ccf4f990aee3ebb1b9375
bchen70 <bchen17@stuy.edu>
2018-04-19 00:12:30 -0400

    finished

--- 09StackCalculator/Calculator.java
+++ 09StackCalculator/Calculator.java
@@ -9,6 +9,10 @@ public class Calculator{
 
     for (String ah : ary){
       char y = ah.charAt(0);
+      if(Character.isDigit(y)){
+        double i = Double.parseDouble(ah);
+        x.push(i);
+      }
       if (y == '+'){
         double a = x.pop();
         double b = x.pop();
@@ -37,4 +41,9 @@ public class Calculator{
     }
     return x.pop();
   }
+  public static void main(String[] args){
+    System.out.println(eval("10 2.0 +"));// is 12.0
+    System.out.println(eval("11 3 - 4 + 2.5 *"));// is 30.0
+    System.out.println(eval("8 2 + 99 9 - * 2 + 9 -")); //is 893.0
+}
 }
