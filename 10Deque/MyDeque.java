729394a97016ba1611b2ef81c75e1a0c5f0d4476
bchen70 <bchen17@stuy.edu>
2018-04-22 17:44:13 -0400

    finally fixed the addFirst lol

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -10,7 +10,6 @@ public class MyDeque<E>{
 	length = 0;
 	first = 0;
 	last = 0;
-	
     }
 
     @SuppressWarnings("unchecked")
@@ -43,11 +42,11 @@ public class MyDeque<E>{
 	if (element == null){
 	    throw new NullPointerException();
 	}
-	else if(size() != 0){
-	    last = (last + 1) % data.length;
-	}
-	else if (size() == data.length){
+	if (size() == data.length){
 	    resize();
+	}
+	if(size() != 0){
+	    first = (first - 1 + data.length) % data.length;
 	}
 	data[first] = element;
 	length ++;
@@ -87,9 +86,90 @@ public class MyDeque<E>{
 	E cur = data[last];
 	data[last] = null;
 	if (length > 1){
-	    last  = (last -1+data.length) % data.length;
+	    last  = (last-1+data.length) % data.length;
 	}
 	length--;
 	return cur;
+    }
+
+    public E getFirst(){
+	if (length < 1){
+	    throw new NoSuchElementException();
+	}
+	return data[first];
+    }
+
+    public E getLast() {
+	if (length < 1) {
+	    throw new NoSuchElementException();
+	}
+	return data[last];
+    }
+
+    /// using Crystal's driver
+    public String toString(){
+    String ans = "[";
+    if(first < last){
+      for (int i = first; i <= last; i++){
+        ans += data[i] + " , ";
+      }
+    }
+    else{
+      for(int i = first; i < data.length; i++){
+        ans += data[i] + ", ";
+      }
+      for(int i = 0; i <= last; i++){
+        ans += data[i] + ", ";
+      }
+    }
+    ans = ans.substring(0, ans.length() - 2) + "]";
+    return ans;
+  }
+
+    public static void main(String[] args) {
+	MyDeque<String> a = new MyDeque<>(), a1 = new MyDeque<>();
+	ArrayList<String> b = new ArrayList<>();
+
+	int size = Integer.parseInt(args[0]);
+	for(int i = 0; i < size; i++){
+	    int temp = (int)(Math.random() * 1000);
+	    if(temp % 2 == 0){
+		a.addFirst("" + temp);
+		a1.addFirst("" + temp);
+		b.add(0, "" + temp);
+	    }
+	    else{
+		a.addLast("" + temp);
+		a1.addLast("" + temp);
+		b.add("" + temp);
+	    }
+	}
+
+	int index = 0;
+	boolean hasError = false;
+	String errorEvaluation = "Errors found at these indices: ";
+	for (String x : b){
+	    if (!(x.equals(a.getFirst()))){
+		System.out.println("The getFirst() function is incorrect at index " + index);
+		hasError = true;
+	    }
+	    if (!(x.equals(a.removeFirst()))){
+		System.out.println("There is an error at index " + index);
+		errorEvaluation += index + ", ";
+		hasError = true;
+	    }
+	    index++;
+	}
+
+
+	if(hasError){
+	    errorEvaluation = errorEvaluation.substring(0, errorEvaluation.length() - 2);
+	    System.out.println(errorEvaluation);
+	    System.out.println("MyDeque: " + a1);
+	    System.out.println("Actual Deque: " + b);
+	}
+	else{
+	    System.out.println("Your deque is bug-free!");
+	}
     }
 }
