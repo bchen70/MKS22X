969a37ba1d6560b5127daefa8766632f58a68305
bchen70 <bchen17@stuy.edu>
2018-04-22 17:01:01 -0400

    WORKING ON ITgit add .

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,7 +1,19 @@
 public class MyDeque<E>{
     private E[] data;
-    @SupressWarnings("unchecked");
+    private int length;
+    
+    @SupressWarnings("unchecked")
     public MyDeque(){
 	data = (E[]) new Object[10];
+	
+    }
+
+    @SupressWarnings("unchecked")
+    public MyDeque(int capacity){
+	data = (E[]) new Object[capcity];
+    }
+
+    public int size(){
+	return length;
     }
 }
