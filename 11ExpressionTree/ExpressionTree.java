fe046c7665b242f9acae5fffe48890812ce6e5db
bchen70 <bchen17@stuy.edu>
2018-04-26 23:53:24 -0400

    finished

--- 11ExpressionTree/ExpressionTree.java
+++ 11ExpressionTree/ExpressionTree.java
@@ -5,37 +5,39 @@ public class ExpressionTree{
     /*return the expression as an infix notation string with parenthesis*/
     /* The sample tree would be: "( 3 + (2 * 10))"     */
     public String toString(){
-	if (
-	str = "(";
-	return "";
+      if (isValue()){
+        return getValue() + "";
+      }
+      return "(" + getLeft().toString() + " " + getOp() + " " + getRight().toString()+ ")";
     }
   
     /*return the expression as a postfix notation string without parenthesis*/
     /* The sample tree would be: "3 2 10 * +"     */
     public String toStringPostfix(){
-	/*you are to write this method*/
-	return "";
+      if (isValue()){
+        return getValue() + " ";
+      }
+      return getLeft().toStringPostfix() + getRight().toStringPostfix() + getOp() + " ";
     }
   
     /*return the expression as a prefix notation string without parenthesis*/
     /* The sample tree would be: "+ 3 * 2 10"     */
   
     public String toStringPrefix(){
-	/*you are to write this method*/
-	return "";
+      if (isValue()){
+        return getValue() + " ";
+      }
+      return getOp() + " " + getLeft().toStringPrefix() + getRight().toStringPrefix();
     }
   
   
   
     /*return the value of the specified expression tree*/
     public double evaluate(){
-	if (isValue()){
-	    return getValue();
-	}else{
-	    double a = getLeft().evaluate();
-	    double b = getRight().evaluate();
-	    return apply(getOp(),a,b);
-	}
+      if (isValue()){
+        return getValue();
+      }else{
+        return apply(getOp(), getLeft().evaluate(), getRight().evaluate());      }
     }
    
     
